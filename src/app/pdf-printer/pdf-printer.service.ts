07c1cca6b84626707eec403ed3919e4c9191e6db
stephan <webmaster1@beyondjava.de>
2021-01-03 23:37:07 +0100

    VadimDez/ng2-pdf-viewer#69 add a print service

--- src/app/pdf-printer/pdf-printer.service.ts
+++ src/app/pdf-printer/pdf-printer.service.ts
@@ -2,13 +2,6 @@ import { Injectable } from '@angular/core';
 import { PDFDocumentProxy } from 'pdfjs-dist';
 import { PDFPageView } from './pdf_page_view';
 
-const userAgent =
-  (typeof navigator !== 'undefined' && navigator.userAgent) || '';
-
-const isIE = /Trident/.test(userAgent);
-
-const isIOSChrome = /CriOS/.test(userAgent);
-
 interface PrintItem {
   width: string;
   height: string;
@@ -24,115 +17,134 @@ interface PageOverview {
   providedIn: 'root',
 })
 export class PdfPrinterService {
-  // Checks if possible to use URL.createObjectURL()
-  private readonly disableCreateObjectURL = isIE || isIOSChrome;
-
-  private currentPage: number = -1;
-  private scratchCanvas: HTMLCanvasElement;
-
-  private printContainer: HTMLIFrameElement;
 
   public getCurrentViewer: () => any;
 
-  private pagesOverview: Array<PageOverview>;
-
-  private enablePrintAutoRotate = true;
-
-  private _printResolution = 300;
-
-  private pages: Array<PDFPageView>;
-
-  private pdfDocument: PDFDocumentProxy;
-
   constructor() {}
 
-  public async print(): Promise<void> {
+  public async print(printResolution = 300, autoRotate = true): Promise<void> {
     const currentViewer = this.getCurrentViewer();
-    this.pages = currentViewer._pages;
-    this.pdfDocument = currentViewer.pdfDocument;
-    this.scratchCanvas = document.createElement('canvas');
-    this.currentPage = -1;
+    const pages = currentViewer._pages;
+    const pdfDocument = currentViewer.pdfDocument;
+    const scratchCanvas = document.createElement('canvas');
     const iframe = document.createElement('iframe');
     iframe.style.display = 'none';
     document.body.appendChild(iframe);
-    this.pagesOverview = this.getPagesOverview();
-    const hasEqualPageSizes = this.pagesOverview.every(function (size) {
-      return (
-        size.width === this.pagesOverview[0].width &&
-        size.height === this.pagesOverview[0].height
-      );
-    }, this);
+    const pagesOverview = this.getPagesOverview(pages, autoRotate);
+    const firstPageSize = pagesOverview[0];
+    const hasEqualPageSizes = pagesOverview.every(
+      (size) =>
+        size.width === firstPageSize.width &&
+        size.height === firstPageSize.height
+    );
     if (!hasEqualPageSizes) {
       console.warn(
-        'Not all pages have the same size. The printed ' +
-          'result may be incorrect!'
+        'Not all pages have the same size. The printed result may be incorrect!'
       );
     }
 
     // Insert a @page + size rule to make sure that the page size is correctly
     // set. Note that we assume that all pages have the same size, because
     // variable-size pages are not supported yet (e.g. in Chrome & Firefox).
-    // TODO(robwu): Use named pages when size calculation bugs get resolved
-    // (e.g. https://crbug.com/355116) AND when support for named pages is
-    // added (http://www.w3.org/TR/css3-page/#using-named-pages).
-    // In browsers where @page + size is not supported (such as Firefox,
-    // https://bugzil.la/851441), the next stylesheet will be ignored and the
-    // user has to select the correct paper size in the UI if wanted.
+
     const pageStyleSheet = document.createElement('style');
-    const pageSize = this.pagesOverview[0];
+    const pageSize = firstPageSize;
     pageStyleSheet.textContent =
       // "size:<width> <height>" is what we need. But also add "A4" because
       // Firefox incorrectly reports support for the other value.
-      '@supports ((size:A4) and (size:1pt 1pt)) {' +
-      '@page { size: ' +
-      pageSize.width +
-      'pt ' +
-      pageSize.height +
-      'pt;}' +
-      '}';
+      `@supports ((size:A4) and (size:1pt 1pt)) {
+        @page { size: ${pageSize.width}px ${pageSize.height}px; }
+       }
+       #printContainer {
+        height: 100%;
+      }
+      /* wrapper around (scaled) print canvas elements */
+      #printContainer, #printContainer * {
+        margin: 0;
+        border: 0;
+        padding: 0;
+      }
+      #printContainer > div {
+        position: relative;
+        top: 0;
+        left: 0;
+        width: 1px;
+        height: 1px;
+        overflow: visible;
+        page-break-after: always;
+        page-break-inside: avoid;
+      }
+      #printContainer canvas,
+      #printContainer img {
+        direction: ltr;
+        display: block;
+      }
+       `;
+    iframe.id = 'printContainer';
     iframe.appendChild(pageStyleSheet);
-    this.printContainer = iframe;
 
-    await this.renderPages();
+    const iFrameBody = iframe.contentDocument.documentElement.querySelector('body');
+    iFrameBody.style.margin="0";
+
+    await this.renderPages(
+      printResolution,
+      pagesOverview,
+      pdfDocument,
+      iFrameBody,
+      scratchCanvas
+    );
 
     iframe.contentWindow.print();
-    console.log("After print");
     document.body.removeChild(iframe);
   }
 
-  private async renderPages(): Promise<void> {
-    const pageCount = this.pagesOverview.length;
-    while (++this.currentPage < pageCount) {
-
-      const index = this.currentPage;
+  private async renderPages(
+    printResolution: number,
+    pagesOverview: Array<PageOverview>,
+    pdfDocument: PDFDocumentProxy,
+    iFrameBody: HTMLBodyElement,
+    scratchCanvas: HTMLCanvasElement
+  ): Promise<void> {
+    const pageCount = pagesOverview.length;
+    let currentPage = -1;
+    while (++currentPage < pageCount) {
       // renderProgress(index, window.filteredPageCount | pageCount, this.l10n, this.eventBus); // #243 and #588 modified by ngx-extended-pdf-viewer
       const printItem = await this.renderPage(
-        /* pageNumber = */ index + 1,
-        this.pagesOverview[index],
-        this._printResolution
+        /* pageNumber = */ currentPage + 1,
+        pagesOverview[currentPage],
+        printResolution,
+        pdfDocument,
+        scratchCanvas
       );
 
-      await this.useRenderedPage(printItem);
+      await this.useRenderedPage(printItem, iFrameBody, scratchCanvas);
     }
   }
 
-  private async useRenderedPage(printItem: PrintItem): Promise<void> {
+  private async useRenderedPage(
+    printItem: PrintItem,
+    iFrameBody: HTMLBodyElement,
+    scratchCanvas: HTMLCanvasElement
+  ): Promise<void> {
+    // Checks if possible to use URL.createObjectURL()
+    const userAgent = (typeof navigator !== 'undefined' && navigator.userAgent) || '';
+    const isIE = /Trident/.test(userAgent);
+    const isIOSChrome = /CriOS/.test(userAgent);
+    const disableCreateObjectURL = isIE || isIOSChrome;
+
     const img = document.createElement('img');
     img.style.width = printItem.width;
     img.style.height = printItem.height;
 
-    const scratchCanvas = this.scratchCanvas;
-    if ('toBlob' in scratchCanvas && !this.disableCreateObjectURL) {
+    if ('toBlob' in scratchCanvas && !disableCreateObjectURL) {
       scratchCanvas.toBlob((blob) => (img.src = URL.createObjectURL(blob)));
     } else {
       img.src = scratchCanvas.toDataURL();
     }
 
-    const iframeBody = this.printContainer.contentDocument.documentElement.querySelector('body');
-
     const wrapper = document.createElement('div');
     wrapper.appendChild(img);
-    iframeBody.appendChild(wrapper);
+    iFrameBody.appendChild(wrapper);
 
     return new Promise(function (resolve, reject) {
       img.onload = () => resolve();
@@ -144,8 +156,11 @@ export class PdfPrinterService {
    * Returns sizes of the pages.
    * @returns {Array} Array of objects with width/height/rotation fields.
    */
-  private getPagesOverview(): Array<PageOverview> {
-    const pagesOverview = this.pages.map(pageView => {
+  private getPagesOverview(
+    pages: Array<PDFPageView>,
+    autoRotate: boolean
+  ): Array<PageOverview> {
+    const pagesOverview = pages.map((pageView) => {
       const viewport = pageView.pdfPage.getViewport({ scale: 1 });
       return {
         width: viewport.width,
@@ -153,7 +168,7 @@ export class PdfPrinterService {
         rotation: viewport.rotation,
       };
     });
-    if (!this.enablePrintAutoRotate) {
+    if (!autoRotate) {
       return pagesOverview;
     }
     return pagesOverview.map(function (size) {
@@ -169,17 +184,16 @@ export class PdfPrinterService {
   }
 
   private async renderPage(
-    pageNumber,
-    size,
-    printResolution
+    pageNumber: number,
+    size: PageOverview,
+    printResolution: number,
+    pdfDocument: PDFDocumentProxy,
+    scratchCanvas: HTMLCanvasElement
   ): Promise<PrintItem> {
-    const scratchCanvas = this.scratchCanvas;
-
     // The size of the canvas in pixels for printing.
     let PRINT_UNITS = printResolution / 72.0;
     const CSS_UNITS = 96.0 / 72.0;
 
-    // modified by ngx-extended-pdf-viewer #387
     let scale = 1;
 
     PRINT_UNITS *= scale;
@@ -190,7 +204,6 @@ export class PdfPrinterService {
     // The physical size of the img as specified by the PDF document.
     const width = Math.floor(size.width * CSS_UNITS) + 'px';
     const height = Math.floor(size.height * CSS_UNITS) + 'px';
-    // end of modification
 
     const ctx = scratchCanvas.getContext('2d');
     ctx.save();
@@ -198,13 +211,13 @@ export class PdfPrinterService {
     ctx.fillRect(0, 0, scratchCanvas.width, scratchCanvas.height);
     ctx.restore();
 
-    const pdfPage = await this.pdfDocument.getPage(pageNumber);
+    const pdfPage = await pdfDocument.getPage(pageNumber);
     const renderContext = {
       canvasContext: ctx,
       transform: [PRINT_UNITS, 0, 0, PRINT_UNITS, 0, 0],
       viewport: pdfPage.getViewport({ scale: 1, rotation: size.rotation }),
       intent: 'print',
-      annotationStorage: (this.pdfDocument as any).annotationStorage,
+      annotationStorage: (pdfDocument as any).annotationStorage,
     };
 
     await pdfPage.render(renderContext).promise;
